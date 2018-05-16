d9870b5cb162d80889295541f035354516614ed9
Reconnois <GUI.QUIRIN@hotmail.fr>
2018-05-16 22:23:11 +0200

    npm install socket.io

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express')
     app = express(),
     server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
     ent = require('ent'); // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
 
 app.use("/searching", express.static(__dirname + '/searching'));
@@ -20,7 +21,6 @@ T.get('account/verify_credentials', { skip_status: true }) //Vérification des i
     // `result` is an Object with keys "data" and "resp".
     // `data` and `resp` are the same objects as the ones passed to the callback.
     // See https://github.com/ttezel/twit#tgetpath-params-callback for details.
- 
     //console.log('data', result.data);
 });
 
@@ -30,31 +30,48 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/twitter');
 var conn = mongoose.connection;
 
-const Cat = mongoose.model('Cat', { name: String });
-
 /* ******************** */
 
 // Chargement de la page index.html
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/searching/index.html');
+    res.sendFile(__dirname + '/searching/index.html');
 });
 
-var params = {
-    q: 'voiture',
-    count: 2,
-    //lang: eu,
-    //result_type: mixed/recent/popular //Filtre des tweets récents et/ou populaires
-    //until: 2015-07-18,
-} 
-// get is the function to search the tweet which three parameters 'search/tweets',params and a callback function.
-T.get('search/tweets', params,searchedData); 
+io.sockets.on('connection', function (socket) {
+
+    //Recherche
+    socket.on('search', async function(datas) {
+        var params = {};
+
+        params.q = datas.keyword;
+
+        if(datas.date_pub != "")
+            params.q += " since:"+datas.date_pub;
+
+        if(datas.quantity != "")
+            params.count = datas.quantity;
+
+        if(datas.type != "")
+            params.result_type = datas.type;
+
+        console.log(params);
+
+        // get is the function to search the tweet which three parameters 'search/tweets',params and a callback function.
+        //T.get('search/tweets', params, searchedData);
+
+        socket.emit('search', {});
+    });
+});
+
 
 // searchedData function is a callback function which returns the data when we make a search
 function searchedData(err, data, response) {
     //Insertion des tweets en BDD
     data.statuses.forEach(function(element){
         element._id = element.id;
-        conn.collection('tweet').insert(element);
+        //element.keyword = ;
+        //conn.collection('tweet').insert(element);
+        console.log(element);
     });
 }
 
