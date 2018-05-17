71cd8ca54b7d4712f2fe7a9f3e6f7018345095ff
sanae <sanai-son112@hotmail.fr>
2018-05-17 11:46:17 +0200

    add new chart

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-//HTML
 var express = require('express')
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
-    ent = require('ent'); // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server),
+  ent = require('ent'); // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
 
 app.use("/searching", express.static(__dirname + '/searching'));
 
 //TWITTER
 var Twit = require('twit'),
-    config = require('./config'),
-    T = new Twit(config);
+  config = require('./config'),
+  T = new Twit(config);
 
 
-T.get('account/verify_credentials', { skip_status: true }) //Vérification des identifiants de l'API
+T.get('account/verify_credentials', {
+    skip_status: true
+  }) //Vérification des identifiants de l'API
   .catch(function (err) {
     console.log('------------- Caught error -------------', err.stack)
   })
@@ -22,7 +23,7 @@ T.get('account/verify_credentials', { skip_status: true }) //Vérification des i
     // `data` and `resp` are the same objects as the ones passed to the callback.
     // See https://github.com/ttezel/twit#tgetpath-params-callback for details.
     //console.log('data', result.data);
-});
+  });
 
 
 //BDD
@@ -34,45 +35,45 @@ var conn = mongoose.connection;
 
 // Chargement de la page index.html
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/searching/index.html');
+  res.sendFile(__dirname + '/searching/index.html');
 });
 
 io.sockets.on('connection', function (socket) {
 
-    //Recherche
-    socket.on('search', async function(datas) {
-        var params = {};
+  //Recherche
+  socket.on('search', async function (datas) {
+    var params = {};
 
-        params.q = datas.keyword;
+    params.q = datas.keyword;
 
-        if(datas.date_pub != "")
-            params.q += " since:"+datas.date_pub;
+    if (datas.date_pub != "")
+      params.q += " since:" + datas.date_pub;
 
-        if(datas.quantity != "")
-            params.count = datas.quantity;
+    if (datas.quantity != "")
+      params.count = datas.quantity;
 
-        if(datas.type != "")
-            params.result_type = datas.type;
+    if (datas.type != "")
+      params.result_type = datas.type;
 
-        console.log(params);
+    console.log(params);
 
-        // get is the function to search the tweet which three parameters 'search/tweets',params and a callback function.
-        //T.get('search/tweets', params, searchedData);
+    // get is the function to search the tweet which three parameters 'search/tweets',params and a callback function.
+    //T.get('search/tweets', params, searchedData);
 
-        socket.emit('search', {});
-    });
+    socket.emit('search', {});
+  });
 });
 
 
 // searchedData function is a callback function which returns the data when we make a search
 function searchedData(err, data, response) {
-    //Insertion des tweets en BDD
-    data.statuses.forEach(function(element){
-        element._id = element.id;
-        //element.keyword = ;
-        //conn.collection('tweet').insert(element);
-        console.log(element);
-    });
+  //Insertion des tweets en BDD
+  data.statuses.forEach(function (element) {
+    element._id = element.id;
+    //element.keyword = ;
+    //conn.collection('tweet').insert(element);
+    console.log(element);
+  });
 }
 
 server.listen(8080);
